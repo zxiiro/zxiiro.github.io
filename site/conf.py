8e5c16907e7dc164341a78ae7fdf3458e320f069
Thanh Ha <zxiiro@linux.com>
2018-04-29 22:03:16 -0400

    Bootstrap Sphinx
    Signed-off-by: Thanh Ha <zxiiro@linux.com>

---
+++ site/conf.py
@@ -0,0 +1,24 @@
+author = 'Thanh Ha'
+copyright = '2018, Thanh Ha'
+project = u'Thanh Ha'
+version = '3.0'
+release = version
+
+extensions = []
+language = None
+master_doc = 'index'
+source_suffix = '.rst'
+templates_path = ['_templates']
+
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+pygments_style = 'sphinx'
+todo_include_todos = False
+
+html_theme = 'alabaster'
+html_theme_options = {
+    'analytics_id': 'UA-46385504-1',
+    'canonical_url': 'http://www.zxiiro.ca/',
+}
+html_static_path = ['_static']
+htmlhelp_basename = 'sitedoc'
+
---
+++ site/index.rst
@@ -0,0 +1,7 @@
+Thanh Ha
+========
+
+I am an Open Source developer working on the OpenDaylight project,
+Release Engineer at the Linux Foundation, and occassional contributor
+to the OpenStreetMap project.
+
