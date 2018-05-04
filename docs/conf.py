b9bdb4e17be584aed4323fd61f9872c6ea4f0a45
Thanh Ha <zxiiro@linux.com>
2018-05-04 00:36:18 -0400

    Switch to Bootstrap theme
    Signed-off-by: Thanh Ha <zxiiro@linux.com>

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,5 @@
+import sphinx_bootstrap_theme
+
 author = 'Thanh Ha'
 copyright = '2018, Thanh Ha'
 project = u'Thanh Ha'
@@ -14,21 +16,27 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 pygments_style = 'sphinx'
 todo_include_todos = False
 
-html_theme = 'alabaster'
+html_theme = 'bootstrap'
+html_theme_path = sphinx_bootstrap_theme.get_html_theme_path()
 html_theme_options = {
-    'analytics_id': 'UA-46385504-1',
-    'canonical_url': 'http://www.zxiiro.ca/',
+    # 'analytics_id': 'UA-46385504-1',
+    # 'canonical_url': 'http://www.zxiiro.ca/',
+
+    #'bootswatch_theme': "litera",
+    'navbar_pagenav': False,
+    'navbar_sidebarrel': False,
+    'source_link_position': "footer",
 }
 
-html_sidebars = {
-    '**': [
-        'about.html',
-        'navigation.html',
-        'relations.html',
-        'searchbox.html',
-        'donate.html',
-    ]
-}
+# html_sidebars = {
+#     '**': [
+#         'about.html',
+#         'navigation.html',
+#         'relations.html',
+#         'searchbox.html',
+#         'donate.html',
+#     ]
+# }
 
 html_static_path = ['_static']
 htmlhelp_basename = 'sitedoc'
