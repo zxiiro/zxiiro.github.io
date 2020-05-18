bbd1681c1b1be5699d83f44987b9bb06bb83796b
Thanh Ha <zxiiro@gmail.com>
2020-05-17 21:36:00 -0400

    Remove Sphinx based website
    Now that we've ported everything remove old files.
    
    Signed-off-by: Thanh Ha <zxiiro@gmail.com>

--- publish.sh
+++ publish.sh
@@ -2,8 +2,12 @@
 
 export PRE_COMMIT_ALLOW_NO_CONFIG=1
 
+git worktree add -f /tmp/site master
+
 cd src && hugo
 cd /tmp/site
 git add .
 git commit -sm "Update site $(date +'%Y%m%d-%H%M')"
 git push
+
+git worktree remove -f /tmp/site
